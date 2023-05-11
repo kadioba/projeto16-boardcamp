ac9852f648b16c76fed77928f9ba3a32f34f8ea8
Vinicius Costa Predebon <kadiobaoff@gmail.com>
2023-05-11 09:30:34 -0300

    feat: adicao do esqueleto das funcoes da rota customers

---
+++ src/controllers/customers.controller.js
@@ -0,0 +1,32 @@
+
+export async function getCustomers(req, res) {
+    try {
+        res.send("Função não implementada")
+    } catch (err) {
+        res.status(500).send(err)
+    }
+}
+
+export async function getCustomerById(req, res) {
+    try {
+        res.send("Função não implementada")
+    } catch (err) {
+        res.status(500).send(err)
+    }
+}
+
+export async function insertCustomer(req, res) {
+    try {
+        res.send("Função não implementada")
+    } catch (err) {
+        res.status(500).send(err)
+    }
+}
+
+export async function updateCustomer(req, res) {
+    try {
+        res.send("Função não implementada")
+    } catch (err) {
+        res.status(500).send(err)
+    }
+}
--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import { getCustomerById, getCustomers, insertCustomer, updateCustomer } from "../controllers/customers.controller.js";
 
 const customersRouter = Router()
 
-customersRouter.get("/customers")
-customersRouter.get("/customers/:id")
-customersRouter.post("/customers")
-customersRouter.put("/customers/:id")
+customersRouter.get("/customers", getCustomers)
+customersRouter.get("/customers/:id", getCustomerById)
+customersRouter.post("/customers", insertCustomer)
+customersRouter.put("/customers/:id", updateCustomer)
 
 export default customersRouter
