9171f2fdc51efe17916eb3f9ac07be419c355da5
Vinicius Costa Predebon <kadiobaoff@gmail.com>
2023-05-11 09:33:52 -0300

    feat: adicao do esqueleto das funcoes da rota games

---
+++ src/controllers/games.controller.js
@@ -0,0 +1,16 @@
+
+export async function getGames(req, res) {
+    try {
+        res.send("Função não implementada")
+    } catch (err) {
+        res.status(500).send(err)
+    }
+}
+
+export async function insertGame(req, res) {
+    try {
+        res.send("Função não implementada")
+    } catch (err) {
+        res.status(500).send(err)
+    }
+}
--- src/routes/games.routes.js
+++ src/routes/games.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import { getGames, insertGame } from "../controllers/games.controller.js";
 
 const gamesRouter = Router()
 
-gamesRouter.get("/games")
-gamesRouter.post("/games")
+gamesRouter.get("/games", getGames)
+gamesRouter.post("/games", insertGame)
 
 export default gamesRouter
