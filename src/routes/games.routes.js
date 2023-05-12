888d01d88734076aed2666889f6d28a4c7d442a6
Vinicius Costa Predebon <kadiobaoff@gmail.com>
2023-05-12 09:58:44 -0300

    fix: adicao do middleware validatescham na rota post games

--- src/routes/games.routes.js
+++ src/routes/games.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { getGames, insertGame } from "../controllers/games.controller.js";
+import { validateSchema } from "../middlewares/validateSchema.middleware.js";
+import { gameSchema } from "../schemas/games.schemas.js";
 
 const gamesRouter = Router()
 
 gamesRouter.get("/games", getGames)
-gamesRouter.post("/games", insertGame)
+gamesRouter.post("/games", validateSchema(gameSchema), insertGame)
 
 export default gamesRouter
